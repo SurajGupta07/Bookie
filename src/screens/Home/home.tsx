8a8d44344465c7b43d7234ba50fc830aafb6f2c9
Suraj BG <surajbg@Suraj-BG-B571s-MacBook-Pro.local>
2022-09-27 19:53:08 +0530

    📝 📝 [Minor] Refactor

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -3,11 +3,15 @@ import {Button, Text, View} from 'react-native';
 import {THome} from './types';
 import {styles} from './style';
 import {APPROUTES} from '../../types/enums';
+import {API_URL} from '@env';
 
 export const Home: React.FC<THome> = ({navigation}) => {
   return (
     <View style={styles.sectionContainer}>
       <Text style={styles.sectionTitle}>Home</Text>
+      <Text>
+        env example {'->'} {API_URL}
+      </Text>
       <Button
         title="Go to details"
         onPress={() => navigation.navigate(APPROUTES.details)}
