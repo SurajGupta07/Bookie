56081aa5387fba202057b4f001bfc9cf778f0f20
Suraj BG <surajbg@Suraj-BG-B571s-MacBook-Pro.local>
2022-11-26 18:19:02 +0530

    🚀 🚀 [Add] App translations

--- src/screens/Home/home.tsx
+++ src/screens/Home/home.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
 
 import {THome} from './types';
 import {styles} from './style';
@@ -8,13 +9,23 @@ import {API_URL} from '@env';
 import {APP_IMAGES} from '../../utils/imageMapper';
 import {AImage} from '../../components/image/image';
 import dimensions from '../../utils/dimensions';
+import {AppDispatch} from '../../redux/store';
+import {getLanguage} from '../../redux/features/language/languageSelectors';
+import {setLanguage} from '../../redux/features/language/languageSlice';
 
 export const Home: React.FC<THome> = ({navigation}) => {
+  const dispatch = useDispatch<AppDispatch>();
+  const language = useSelector(getLanguage);
+
+  const disptachSetLanguage = (selectedLanguage: string) => {
+    dispatch(setLanguage(selectedLanguage));
+  };
+
   return (
     <View style={styles.sectionContainer}>
-      <Text style={styles.sectionTitle}>Home</Text>
+      <Text style={styles.sectionTitle}>{language.homeScreen.home}</Text>
       <Text>
-        env example {'->'} {API_URL}
+        {language.homeScreen.env} {'->'} {API_URL}
       </Text>
       <Button
         title="Go to details"
@@ -24,6 +35,14 @@ export const Home: React.FC<THome> = ({navigation}) => {
         image={APP_IMAGES.booking}
         imageWidth={dimensions.viewWidth(200)}
         imageHeight={dimensions.viewWidth(200)}
+      />
+      <Button
+        title="Set Language as Hindi"
+        onPress={() => disptachSetLanguage('Hindi')}
+      />
+      <Button
+        title="Set Language as English"
+        onPress={() => disptachSetLanguage('English')}
       />
     </View>
   );
